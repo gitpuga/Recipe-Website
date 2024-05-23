aa2b673bca89c3fa91c298a2824aaeabce40f448
Svyatozar Naumov <svyatozar1@gmail.com>
2024-05-23 19:30:39 +0300

    modified: App.js & deleted: UserPage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Login from "./components/Login"
+import Register from "./components/Register";
+import Login from "./components/Login";
+import RecipeList from "./components/RecipeList";
 import HomePage from "./pages/HomePage";
 import AddRecipePage from "./pages/AddRecipePage";
 import RecipePage from "./pages/RecipePage";
 import ContactPage from "./pages/ContactPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import UserPage from "./pages/UserPage";
 import "./styles/App.css";
 import "./styles/Accessibility.css";
 
+
 function App() {
   const [isAccessibilityMode, setAccessibilityMode] = useState(false);
+  const [searchTerm] = useState("");
+  const [ingredientFilter] = useState("");
+  const [difficultyFilter] = useState("");
   const [user, setUser] = useState(null);
 
   const toggleAccessibilityMode = () => {
@@ -32,29 +37,32 @@ function App() {
           <div className="content">
             <main>
               <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route
-                  path="/"
-                  element={<HomePage />}
+                  path="/add-recipe"
+                  element={
+                    user ? <AddRecipePage /> : <Login onLogin={handleLogin} />
+                  }
                 />
-                <Route 
-                  path="/add-recipe" 
-                  element={user ? <AddRecipePage /> : <Login onLogin={handleLogin} />} 
-                />
-                <Route 
-                  path="/recipe/:id" 
-                  element={<RecipePage />} 
-                />
-                <Route 
-                  path="/contact" 
-                  element={<ContactPage />} 
-                />
-                <Route 
-                  path="*" 
-                  element={<NotFoundPage />} 
-                />
-                <Route 
-                  path="/user" 
-                  element={<UserPage />} 
+                <Route path="/recipe/:id" element={<RecipePage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+                <Route
+                  path="/user"
+                  element={
+                    user ? (
+                      <RecipeList
+                        searchTerm={searchTerm}
+                        ingredientFilter={ingredientFilter}
+                        difficultyFilter={difficultyFilter}
+                      />
+                    ) : (
+                      <div>
+                        <Register />
+                        <Login onLogin={handleLogin} />
+                      </div>
+                    )
+                  }
                 />
               </Routes>
             </main>
