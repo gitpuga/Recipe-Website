fe6ea5b0aa9221963dc16204641e999d8333122f
Svyatozar Naumov <svyatozar1@gmail.com>
2024-05-23 14:14:31 +0300

    modified: styling & dockerfiles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-left-container">
-        <nav className="header-nav">
+        <nav className="nav-container">
           <Link className="a" to="/">
-            <h1 className="header-text">🍽️ CookBook</h1>
+            <h1 className="header-title-text">🍽️ CookBook</h1>
           </Link>
         </nav>
-      </div>
-      <nav className="header-nav">
+        <div className="header-divider" />
         <div className="nav-container">
           <Link className="a" to="/add-recipe">
-            <h1 className="header-text">+ добавить рецепт</h1>
+            <h1 className="header-text">добавить рецепт</h1>
           </Link>
         </div>
         <div className="nav-container">
           <Link className="a" to="/contact">
-            <h1 className="header-text">☎ контакты</h1>
+            <h1 className="header-text">контакты</h1>
           </Link>
         </div>
+      </div>
+      <nav className="header-right-container">
+        <button className="header-user-button">
+          <FaUser size={20} className="header-user-icon" />
+        </button>
       </nav>
     </header>
   );
